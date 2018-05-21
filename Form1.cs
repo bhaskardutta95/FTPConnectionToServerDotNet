2234f5ddd794de5d92aecbc9721cda6d3c422b83
bhaskardutta95 <36147983+bhaskardutta95@users.noreply.github.com>
2018-05-21 12:41:31 +0530

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace LeapProject_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Connection conn = new Connection(@"ftp://155.17.173.56/", "testtenethealth\bhaskar.dutta", "Tenet123");
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileSize = conn.getFileSize("D:\\Program Files\\IBM\\WebSphere\\AppServer\\profiles\\AppSrv01\\logs\\HDI-iTest-AppSrv02\\SystemOut.txt");
+            textBox1.Text = fileSize;
+        }
+    }
+}
